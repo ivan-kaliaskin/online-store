1e64239904d811cbd64200cce7ed8a7761f02388
Aleksandr Metsler <karantin61@gmail.com>
2022-12-23 20:58:30 +0300

    feat: add app styles

--- src/components/PageContainer.ts
+++ src/components/PageContainer.ts
@@ -4,6 +4,7 @@ import HomePage from "../pages/HomePage"
 function PageContainer(): HTMLElement {
     const main: HTMLElement = document.createElement('main')
     main.setAttribute('id', 'page-container')
+    main.classList.add('main')
     elements.pageContainer = main
     main.append(HomePage())
     return main
