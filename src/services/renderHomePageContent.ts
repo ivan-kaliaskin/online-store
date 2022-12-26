7f3b70d1d9884484dbe85ad13fa0a233471580d9
ivan-kaliaskin <jabr.don24@gmail.com>
2022-12-26 12:05:49 +0400

    feat: font Roboto attached to body

--- src/services/renderHomePageContent.ts
+++ src/services/renderHomePageContent.ts
@@ -1,6 +1,6 @@
 import elements from "../constants/elements"
 import Item from "../store/classItem"
-import items from "../store/itemsArray";
+import itemsArray from "../store/itemsArray";
 
 function renderHomePageContent() {
     fetch('https://dummyjson.com/products')
@@ -9,12 +9,10 @@ function renderHomePageContent() {
             return result
         })
         .then(result => {
-            const itemsFromServer = (result).products.map(product => product.title)
+            itemsArray.itemList = result.products
 
-            items.itemList = itemsFromServer
-
-            const itemsElements: Node[] = itemsFromServer.map(el => {
-                const itemCard: HTMLDivElement = new Item(el)._element
+            const itemsElements: Node[] = result.products.map(el => {
+                const itemCard: HTMLDivElement = new Item(el.title)._element
                 itemCard.classList.add('item-card')
                 return itemCard
             })
