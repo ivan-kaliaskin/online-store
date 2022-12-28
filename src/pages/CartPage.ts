443ddf4272499a8431a643a339941ac1453f359e
ivan-kaliaskin <jabr.don24@gmail.com>
2022-12-28 17:21:32 +0400

    feat: added normal layout into cart page

--- src/pages/CartPage.ts
+++ src/pages/CartPage.ts
@@ -12,12 +12,13 @@ function CartPage() {
     productsInCartTitle.setAttribute('id', 'products-in-cart-container-title')
     productsInCartTitle.innerText = 'Products In Cart'
 
-    const productsInCartList = document.createElement('ul')
+    const productsInCartList = document.createElement('ol')
 
     cart.selectedItems.forEach((el) => {
-        const elInList = document.createElement('li')
+        const elInList = document.createElement('li') // один товар
         elInList.classList.add('product-in-list')
-        elInList.innerText = `${el.item.title}`
+        elInList.innerText = `${el.item.title}. Description: ${el.item.description}. Rating: ${el.item.rating}`
+        // другие свойства объекта item можно достать таким же образом: el.item... vscode подскажет какие существуют
         productsInCartList.append(elInList)
     })
 
@@ -25,6 +26,32 @@ function CartPage() {
 
     const summary = document.createElement('div')
     summary.setAttribute('id', 'summary')
+
+    const summaryTitle = document.createElement('h2')
+    summaryTitle.setAttribute('id', 'summary-title')
+    summaryTitle.innerText = 'Summary'
+
+    const productsAmount = document.createElement('div') // количество товаров
+    productsAmount.setAttribute('id', 'summary-amount')
+    productsAmount.innerText = `Products: ${cart.selectedItems.length}`
+
+    const totalCost = document.createElement('div') // итоговая сумма
+    totalCost.setAttribute('id', 'total-cost')
+    let ntotalCost = 0
+    cart.selectedItems.forEach((el) => {
+        ntotalCost += el.item.price!
+    })
+    totalCost.innerText = `Total: ${ntotalCost}`
+
+    const promocodeInput = document.createElement('input')
+    promocodeInput.setAttribute('id', 'promocode-input')
+    promocodeInput.setAttribute('value', 'enter promo code')
+
+    const buyNowButton = document.createElement('button')
+    buyNowButton.setAttribute('id', 'buy-now-button')
+    buyNowButton.innerText = 'BUY NOW'
+
+    summary.append(summaryTitle, productsAmount, totalCost, promocodeInput, buyNowButton)
 
 
     itemsContainer.append(productsInCartContainer, summary)
