ceaa5e7d630d3471fcd08c321dd8f6cb9c374436
ivan-kaliaskin <jabr.don24@gmail.com>
2022-12-29 10:03:59 +0400

    feat: layout for button block implemented

--- src/components/ProductInCartList.ts
+++ src/components/ProductInCartList.ts
@@ -3,6 +3,7 @@ import SelectedItem from "../interfaces_and_types/TypeSelectedItem"
 function ProductInCartList(product: SelectedItem, index: number) {
     const productInList = document.createElement('li') // один товар
     productInList.classList.add('product-in-cart-list')
+    productInList.setAttribute('id', `product-in-list-${product.item.id}`)
 
     const orderNumber = document.createElement('div') // порядковый номер товара
     orderNumber.classList.add('picl-number')
@@ -22,10 +23,29 @@ function ProductInCartList(product: SelectedItem, index: number) {
 
     const cntStock = document.createElement('div') // в счётчике - количество на складе
     cntStock.classList.add('cnt-stock')
+    cntStock.innerText = `Stock: ${product.item.stock}`
+
     const cntButtonBlock = document.createElement('div') // в счётчике - блок с кнопками +/- и количеством между ними
     cntButtonBlock.classList.add('cnt-button-block')
+
+    const cntButtonBlockButtonPlus = document.createElement('button') // в счётчике - кнопка +
+    cntButtonBlockButtonPlus.classList.add('cnt-btn-block-btn')
+    cntButtonBlockButtonPlus.innerText = '+'
+
+    const cntButtonBlockAmount = document.createElement('div') // в счётчике - количество
+    cntButtonBlockAmount.classList.add('cnt-btn-block-amount')
+    cntButtonBlockAmount.innerText = `${product.amount}`
+
+    const cntButtonBlockButtonMinus = document.createElement('button') // в счётчике - кнопка -
+    cntButtonBlockButtonMinus.classList.add('cnt-btn-block-btn')
+    cntButtonBlockButtonMinus.innerText = '-'
+
+    cntButtonBlock.append(cntButtonBlockButtonPlus, cntButtonBlockAmount, cntButtonBlockButtonMinus)
+
+
     const cntTotal = document.createElement('div') // в счётчике - стоимость данного продукта
     cntTotal.classList.add('cnt-total')
+    cntTotal.innerText = `$ ${(product.item.price || 0) * product.amount}`
 
     productCounter.append(cntStock, cntButtonBlock, cntTotal)
     // --- end counter block --------------------------------------------------------------------------------------
